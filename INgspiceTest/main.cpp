6996b1513d3a0cdceaca150457f6d594148dad7a
huangws <huang.vincent@foxmail.com>
2015-04-24 16:54:38 +0800

    add main entry.

---
+++ INgspiceTest/main.cpp
@@ -0,0 +1,16 @@
+#include "stdafx.h"
+
+#include "test-ingspice.h"
+#include "test-sim.h"
+
+int main (int argc, char **argv)
+{
+	return run_parallel();
+	//return load_error();
+	//return load_error2();
+	//return run_error();
+	//return test_rc();
+	//return test_3p_effective_voltage();
+	//return test_xspice();
+	return 0;
+}
