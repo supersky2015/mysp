4f64f3e3a5e72e72cb1c103753f46ed85a704967
vincent <vcnt518@gmail.com>
2015-05-14 14:47:49 +0800

    remove spaces

--- ingspice/include/plot.h
+++ ingspice/include/plot.h
@@ -23,29 +23,29 @@ struct plot
 	 * 下面成员具有相同的veccount值.
 	 *		pvecs,currentValues,names,pvalues,values 
 	 */
-	int veccount;		
+	int veccount;
 
 	/*
 	 * 各个向量的计算值的个数， 对应于VecPArray, VecArray的size 
 	 * 注：一个plot里面所有向量都有一样size的value
 	 */
-	int vecsize;		
+	int vecsize;
 
 	//初始化ngspice分配的结构指针
 	pvecinfo* pvecs;
 
 	//存储所有向量当前值，方便取用
-	vector<double> currentValues;		
+	vector<double> currentValues;
 	
 	//存储所有向量名
-	vector<string> names;	
+	vector<string> names;
 	
 	/*
 	 * 一个向量历次计算值数组,长度为vecsize, 使用指针可以共享ngspice计算值存储空间 - 定义为 VecArray类型
 	 * pvalues 存储所有向量VecArray
 	 */
-	typedef double* VecPArray;			
-	vector<VecPArray> pvalues;			
+	typedef double* VecPArray;
+	vector<VecPArray> pvalues;
 	
 	/*
 	 * values 等同于 pvalues，不同之处在于 pvalues指向ngspice分配的内存，values自己分配管理
